5bcc8d8731377a1aff2cd9f761222357a950a17f
林嵩嵛 <647851381@qq.com>
2022-09-02 23:49:50 +0800

    提交一个hello

---
+++ untitled/src/com/Hello.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author lin
+ * @create 2022-09-02 23:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
